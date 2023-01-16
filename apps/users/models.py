0813c76c0a2e52c6b6b4a706e9056fe3bffb438c
bibek <beebayk0001@gmail.com>
2023-01-16 20:28:38 +0545

    feat: create manager for user

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,2 +1,26 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+
+from be.libs.models import TimeStampModel
+
+
+class BaseUser(TimeStampModel, AbstractBaseUser, PermissionsMixin):
+
+    USER_TYPE_CHOICES = [
+        ('public', 'public'),
+        ('refugee', 'refugee'),
+        ('volunteer', 'volunteer'),
+        ('manager', 'manager'),
+        ('organization user', 'organization user'),
+
+    ]
+
+    username = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(max_length=255, choices=USER_TYPE_CHOICES)
+
+    USERNAME_FIELD = 'username'
+
+
+
 
